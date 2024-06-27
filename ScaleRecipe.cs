febb349a24fbb72d15e25b59caa47feeed83242f
snazohappymgidi <st10220472@imconnect.edu.za>
2024-06-27 14:52:26 +0200

    added coloour to the output

--- ScaleRecipe.cs
+++ ScaleRecipe.cs
@@ -1,17 +1,21 @@
 ﻿using System.Collections.Generic;
 using System.Windows;
+
 namespace RecipeApp
 {
     public partial class ScaleRecipeWindow : Window
     {
         private Recipe recipe;
+
         public ScaleRecipeWindow(Recipe recipe)
         {
             InitializeComponent();
             this.recipe = recipe;
+
             ScaleFactorComboBox.ItemsSource = new List<double> { 0.5, 2, 3 };
             ScaleFactorComboBox.SelectedIndex = 0;
         }
+
         private void Scale_Click(object sender, RoutedEventArgs e)
         {
             if (ScaleFactorComboBox.SelectedItem is double factor)
@@ -24,6 +28,7 @@ namespace RecipeApp
                 MessageBox.Show("Please select a scaling factor.");
             }
         }
+
         private void Cancel_Click(object sender, RoutedEventArgs e)
         {
             Close();
